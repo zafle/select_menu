fc6c128b70cb01412eea6ff0a0d472d2efe83703
zafle <fleutotanne@free.fr>
2025-03-21 17:22:07 +0100

    fix script bug to run vitest test and coverage (add --config)

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -3,21 +3,11 @@ import { coverageConfigDefaults } from 'vitest/config'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
-// import prism from 'vite-plugin-prismjs'
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    // prism({
-    //   languages: ['javascript', 'css', 'html', 'typescript'],
-    //   plugins: ['line-numbers'],
-    //   theme: 'tomorrow',
-    //   css: true,
-    // }),
-  ],
+  plugins: [react(), libInjectCss()],
   build: {
     lib: {
       entry: resolve('lib/main.js'),
@@ -37,6 +27,9 @@ export default defineConfig({
         '**/src/**',
         '**/lib/tests/**',
         '**/lib/main.js',
+        '**/site-dist/**',
+        '**/vite.lib.config.js',
+        '**/vite.site.config.js',
         ...coverageConfigDefaults.exclude,
       ],
       reporter: ['text', 'json', 'html', 'lcov'],
